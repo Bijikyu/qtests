008440413de9a5522ee4738d6ae2f73334fe4718
Q <qontact@protonmail.com>
2025-06-07 13:39:03 -0700

    refactor tests: replace temp console helper

--- test/logUtils.test.js
+++ test/logUtils.test.js
@@ -1,9 +1,8 @@
 
 
-const { logStart, logReturn } = require('../lib/logUtils'); //functions under test
 const { withMockConsole } = require('../utils/testHelpers'); //(helper for console spies)
 
-const { logStart, logReturn, executeWithLogs } = require('../lib/logUtils'); //functions under test
+const { logStart, logReturn, executeWithLogs } = require('../lib/logUtils'); //functions under test, includes executeWithLogs
 const { mockConsole } = require('../utils/mockConsole'); //capture console output
 
 
